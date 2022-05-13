2ee2bfd2d7f27d491601a2b4d257e8947eecd8ec
santipu_ <santipujolsau@gmail.com>
2022-05-13 12:35:47 +0200

    Created initial environment to work

---
+++ main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document.querySelector('div').textContent = 'gilipollas'
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlcmFwcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCdkaXYnKS50ZXh0Q29udGVudCA9ICdnaWxpcG9sbGFzJ1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
