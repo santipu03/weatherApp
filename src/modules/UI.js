aecef83fc094b095fec237f407c6f270cc577154
santipu_ <santipujolsau@gmail.com>
2022-05-17 13:12:17 +0200

    Created functions to display and clear error msg

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -8,6 +8,7 @@ const feelsLikeTemp = document.querySelector('.feel-temp')
 const humidity = document.querySelector('.humidity')
 const wind = document.querySelector('.wind')
 const clouds = document.querySelector('.clouds')
+const errorMsg = document.querySelector('.error')
 
 const renderCityWeather = (data, unit) => {
   try {
@@ -17,6 +18,7 @@ const renderCityWeather = (data, unit) => {
     clouds.textContent = `${data.clouds.all}%`
     icon.innerHTML = getIcon(data.weather[0].icon)
     renderBackgroundImg(data.weather[0].icon)
+    clearErrorMsg()
 
     if (unit === 'metric') {
       temperature.innerHTML = `${Math.floor(data.main.temp)}<span>°C</span>`
@@ -28,8 +30,15 @@ const renderCityWeather = (data, unit) => {
       wind.textContent = `${data.wind.speed} mph`
     }
   } catch (e) {
-    alert(data.message)
+    renderErrorMsg(data.message)
   }
+}
+
+const clearErrorMsg = () => errorMsg.classList.remove('show')
+
+const renderErrorMsg = (msg) => {
+  errorMsg.classList.add('show')
+  errorMsg.textContent = msg
 }
 
 const renderBackgroundImg = (code) => {
